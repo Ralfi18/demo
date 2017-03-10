d6ca2340442276e96b505670573bc0da47dea2fe
Rali <ralidimitrovdimitrov@gmail.com>
2017-03-10 18:15:51 +0200

    Commit 2 br1

--- index.php
+++ index.php
@@ -20,5 +20,6 @@ function test()
     <h1>Testing the GIT</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dignissimos dolor eveniet in iste mollitia odit, placeat provident tempore veritatis? Architecto aut eaque ipsam molestiae nostrum optio perferendis quas sapiente.</p>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium delectus esse, eum illo molestias neque non nostrum odit perspiciatis quia quibusdam rem reprehenderit sint sunt suscipit tempore tenetur velit veniam.</p>
+<p>Tralala </p>
 </body>
 </html>
